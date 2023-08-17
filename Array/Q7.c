9853e95763bc924cd9055729d1625610bb14d0cb
rahulranjann <rahul1913111054@gmail.com>
2023-08-17 20:46:27 +0530

    update

---
+++ Array/Q7.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void printArray(int *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d \t", arr[i]);
+    }
+}
+
+void rotate(int *arr, int size)
+{
+    int j = size-2;
+    int temp;
+    for (int i = size-1 ; i >=0; i--)
+    {
+        if (j<0)
+        {
+          break;
+        }
+        
+        temp = arr[i];
+        arr[i]= arr[j];
+        arr[j]= temp;
+        j--;
+    }
+    printArray(arr, size);
+}
+
+int main() {
+
+   int size;
+   printf("Enter size of Array\n");
+   scanf("%d",&size);
+   int arr[size];
+   printf("Enter elements of array\n");
+   for (int i = 0; i < size; i++)
+   {
+    scanf("%d",&arr[i]);
+   }
+   rotate(arr, size);
+   
+   return 0;
+}
---
+++ Array/Q8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+   int size;
+   printf("Enter size of array\n");
+   scanf("%d",&size);
+   int arr[size];
+   for (size_t i = 0; i < count; i++)
+   {
+    /* code */
+   }
+   
+   printf
+   return 0;
+}
